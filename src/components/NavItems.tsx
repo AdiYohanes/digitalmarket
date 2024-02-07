9dcee106a7e436f192936ece9aaa93f07ae0e0c3
Adi Yohanes <adiyohanes19@gmail.com>
2024-02-07 10:57:44 +0700

    final email and stripe problem

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useEffect, useRef, useState } from "react";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+
+const NavItems = () => {
+    const [isActive, setIsActive] = useState<null | number>(null)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) =>{
+            if(e.key === "Escape"){
+                setIsActive(null)
+            }
+        }
+
+        document.addEventListener("keydown",handler)
+
+        return () => {
+            document.removeEventListener("keydown",handler)
+        }
+    },[])
+
+    const isAnyOpen = isActive !== null
+
+    const navRef = useRef<HTMLDivElement | null>(null)
+
+    useOnClickOutside(navRef, () => setIsActive(null))
+
+
+  return <div className="flex gap-4 h-full" ref={navRef}>
+    {PRODUCT_CATEGORIES.map((category, i) => {
+
+        const handleOpen = () =>{
+            if(isActive === i){
+                setIsActive(null)
+            }else  {
+                setIsActive(i)
+            }
+        }
+
+        const isOpen = i === isActive
+
+        return(
+            <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}/>
+        )
+    })}
+    </div>;
+};
+
+export default NavItems;
